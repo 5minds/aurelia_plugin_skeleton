f7a2c4723367aa2195cf2a77ffab69a08728054d
bastian schnittger <bastian.schnittger@5minds.de>
2017-08-11 10:01:19 +0200

    edelete custom element specification

--- src/example/elements/my-example.ts
+++ src/example/elements/my-example.ts
@@ -1,6 +1,5 @@
 import {bindable, customElement} from 'aurelia-framework';
 
-@customElement('my-example-tag')
 export class MyExample {
 
   @bindable() public something: string = 'MyExample works';
